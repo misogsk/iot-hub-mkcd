00cd03c3465db2edefb42ea5ab63f2a6bf0fd152
michal grezdo <michal.grezdo@accenture.com>
2023-02-21 14:16:20 +0100

    uopdated

--- iothub.ts
+++ iothub.ts
@@ -51,8 +51,8 @@ namespace esp8266_mg {
     //% weight=29
     //% blockGap=8
     //% blockId=esp8266_send_iothub_message
-    //% block="send message to iothub:|API Key %apiKey|Chat ID %chatId|Message %message"
-    export function sendIotHubMessage(apiKey: string, id: string, message: string) {
+    //% block="send message to iothub:|Host %host
+    export function sendIotHubMessage(host: string) {
 
         // Reset the upload successful flag.
         iotHubMessageSent = false
@@ -63,6 +63,26 @@ namespace esp8266_mg {
         basic.pause(100)
         if (iotConnected) {
             let str: string = "GET /GetFunction?name=esp"
+            sendAT("AT+CIPSEND=" + (str.length + 2))
+            sendAT(str, 0) // upload data
+            iotHubMessageSent = waitResponse()
+            basic.pause(100)
+        }
+    }
+    /**
+    * Connect to ThingSpeak and upload data. It would not upload anything if it failed to connect to Wifi or ThingSpeak.
+    */
+    //% block="Upload data to ThingSpeak|URL/IP = %ip|Write API key = %write_api_key|Field 1 = %n1|Field 2 = %n2|Field 3 = %n3|Field 4 = %n4|Field 5 = %n5|Field 6 = %n6|Field 7 = %n7|Field 8 = %n8"
+    //% ip.defl=api.thingspeak.com
+    //% write_api_key.defl=your_write_api_key
+    export function connectThingSpeak(ip: string, write_api_key: string, n1: number, n2: number, n3: number, n4: number, n5: number, n6: number, n7: number, n8: number) {
+        iotHubMessageSent = false
+        sendAT("AT+CIPSTART=\"TCP\",\"" + ip + "\",80", 0) // connect to website server
+        iotConnected = waitResponse()
+        basic.pause(100)
+        if (iotConnected) {
+            iotHubMessageSent = false
+            let str: string = "GET /update?api_key=" + write_api_key + "&field1=" + n1 + "&field2=" + n2 + "&field3=" + n3 + "&field4=" + n4 + "&field5=" + n5 + "&field6=" + n6 + "&field7=" + n7 + "&field8=" + n8
             sendAT("AT+CIPSEND=" + (str.length + 2))
             sendAT(str, 0) // upload data
             iotHubMessageSent = waitResponse()
